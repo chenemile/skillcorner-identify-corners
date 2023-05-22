5a86a835efd8a41f534738cc7b6a21ec6b0bc888
Emily <chenemile@gmail.com>
2023-05-22 14:05:43 -0700

    i forgot this was a github repo

---
+++ analyze.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+-----------------------------------------------------------------------------
+Submission for the SRC FTBL take-home task for the Data Scientist position
+  Identifies possible corner kick situations in the given SkillCorner dataset
+-----------------------------------------------------------------------------
+:author: Emily Chen
+:date:   May 2023
+
+
+REQUIREMENTS: pandas, numpy, matplotlib, ffmpeg
+
+USAGE: python3 analyze.py [path-to-dir-containing-match-data]
+  e.g. python3 analyze.py data/matches/3442
+
+"""
+from identify_corners import identify_corner_kicks
+
+import argparse
+import glob
+import json
+import os
+import sys
+
+import pandas as pd
+import warnings
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+# for debugging purposes
+pd.set_option("display.max_columns", None)
+np.set_printoptions(threshold=sys.maxsize)
+
+warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
+
+
+def get_player_ids(match_info):
+    '''
+    :param match_info: match_data.json
+    :type  match_info: pandas dataframe
+
+    :return: home_players, away_players
+    :rtype:  list
+
+    Gets each player's tracking ID stored in "trackable_object"
+    and assigns them to either the home team or away team.
+
+    '''
+    home_team_id = match_info["home_team"]["id"]
+    away_team_id = match_info["away_team"]["id"]
+
+    home_players = []
+    away_players = []
+
+    for player in match_info["players"]:
+        if player["team_id"] == home_team_id:
+            home_players.append(player["trackable_object"])
+        elif player["team_id"] == away_team_id:
+            away_players.append(player["trackable_object"])
+
+    return home_players, away_players
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('match_dir', help='path to directory containing match_data.json and structured_data.json')
+    args = parser.parse_args()
+
+    # creates a folder 'analysis' to store output
+    dirname = (args.match_dir).rsplit("/", 1)[1] + "_analysis"
+    if os.path.exists(dirname):
+        for root, dirs, files in os.walk(dirname):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(dirname)
+    os.mkdir(dirname)
+
+    # ---------------------------------
+    # read in data and identify corners
+    # ---------------------------------
+    home_team_ids = []
+    away_team_ids = [] 
+
+    for jsonfile in glob.glob(args.match_dir + "/*.json"):
+        with open(jsonfile, "r") as json_data:
+
+            basename = jsonfile.rsplit("/", 1)[1]
+
+            if basename == "match_data.json":
+                match_data = json.load(json_data)
+                home_team_ids, away_team_ids = get_player_ids(match_data)
+
+            corner_kicks = []
+            if basename == "structured_data.json":
+                tracking_data = pd.read_json(json_data)
+                corner_kicks  = identify_corner_kicks(tracking_data)
+
+    # ----------------------------------
+    # plot corner kicks as animated gifs
+    # ----------------------------------
+
+    # assumes a corner kick occurs once per minute, e.g.
+    #   the frame at time 1:23:10 is assumed to be the same
+    #   corner kick as the frame at time 1:23:80
+    prev_min = 0
+
+    for kick in corner_kicks:
+
+        minute = int(kick["time"].split(":")[0])
+
+        # logs the team ('home' or 'away') of each trackable object
+        #   assists with color-coding players in animated gif
+        team_assignment = pd.DataFrame()
+
+        if prev_min != minute:
+
+            # create dataframes that store the frame, time, and x or y coordinates
+            # of all the trackable objects in each frame
+            #
+            #     Frame  Time      Obj0       Obj1       Obj2       ...
+            #     11823  15:45.00  [x-coord]  [x-coord]  [x-coord]  ...
+            #     11824  15:45.10  [x-coord]  [x-coord]  [x-coord]  ...
+            #
+            #     Frame  Time      Obj0       Obj1       Obj2       ...
+            #     11823  15:45.00  [y-coord]  [y-coord]  [y-coord]  ...
+            #     11824  15:45.10  [y-coord]  [y-coord]  [y-coord]  ...
+            #
+            # dataframes are used for scatterplotting
+            #
+            x_positions = pd.DataFrame(columns = ["Frame", "Time"])
+            y_positions = pd.DataFrame(columns = ["Frame", "Time"])
+    
+            frame_number = kick["frame"]
+    
+            # get the corner kick frame and all frames within 30 seconds after
+            df_for_analysis = tracking_data.iloc[frame_number:frame_number+300]
+    
+            # just in case, for objects with no tracking_id
+            no_id_counter = 0
+    
+            for frame in range(len(df_for_analysis)):
+                data = df_for_analysis.iloc[frame]["data"]
+    
+                # add current frame and time to 'x_positions' and 'y_positions'
+                frame_time = {"Frame": df_for_analysis.iloc[frame]["frame"], \
+                              "Time": df_for_analysis.iloc[frame]["time"]}
+                frame_time_df = pd.DataFrame(frame_time, index=[0])
+    
+                x_positions = pd.concat([x_positions, frame_time_df])
+                y_positions = pd.concat([y_positions, frame_time_df])
+    
+                # check if there's tracking data and a trackable object
+                if data and any("trackable_object" in elem for elem in data):
+    
+                    # get the id, x-y coordinates of the trackable object
+                    for elem in data:
+                        if "trackable_object" in elem:
+                            tracking_id = elem["trackable_object"]
+                        else:
+                            tracking_id = "nan" + str(no_id_counter)
+                            no_id_counter += 1
+
+                        x_coord = elem["x"]
+                        y_coord = elem["y"]
+    
+                        # update 'x_positions' and 'y_positions'
+                        if tracking_id not in x_positions.columns:
+                            x_positions[tracking_id] = ''
+    
+                        if tracking_id not in y_positions.columns:
+                            y_positions[tracking_id] = ''
+    
+                        x_positions.iat[len(x_positions.index)-1, x_positions.columns.get_loc(tracking_id)] = x_coord
+                        y_positions.iat[len(y_positions.index)-1, y_positions.columns.get_loc(tracking_id)] = y_coord
+
+                        # update team assignments
+                        if tracking_id not in team_assignment.columns:
+                            if tracking_id == 55:
+                                team_assignment["55"] = ["ball"]
+                            elif tracking_id in home_team_ids:
+                                team_assignment[tracking_id] = ["home"]
+                            elif tracking_id in away_team_ids:
+                                team_assignment[tracking_id] = ["away"]
+                            else:
+                                team_assignment[tracking_id] = ["unknown"]
+    
+            # convert 'x_positions' and 'y_positions' to numpy arrays,
+            # excluding 'Frame' and 'Time' columns
+            x_over_time = x_positions[x_positions.columns[2:]].to_numpy()
+            y_over_time = y_positions[y_positions.columns[2:]].to_numpy()
+    
+            x_over_time[x_over_time == ''] = np.nan
+            y_over_time[y_over_time == ''] = np.nan
+
+            # map 'team_assignment" to colors
+            colors = {"ball": "red", "home": "blue", "away": "orange", "unknown": "black"}
+            team_colors = team_assignment.squeeze().map(colors)
+    
+            # initialize figure for scatterplot
+            # mark x-y axes
+            fig = plt.figure()
+            ax = plt.axes(xlim=(-52.5,52.5), ylim=(-34,34))
+
+            scatterplot = ax.scatter(x_over_time[0], y_over_time[0], \
+                                     s=8, c=team_colors)
+    
+            def update(frame_number):
+                x = x_over_time[frame_number]
+                y = y_over_time[frame_number]
+
+                x_y = np.zeros(shape=(len(x), 2))
+
+                for i in range(len(x)):
+                    x_y[i] = [x[i], y[i]]
+
+                scatterplot.set_offsets(x_y)
+                return scatterplot,
+    
+            anim = FuncAnimation(fig, update, interval=1)
+            anim.save(dirname + "/minute_" + str(minute) + ".gif", writer = "ffmpeg", fps = 10)
+
+            prev_min = minute
+
+
+if __name__ == "__main__":
+    main()
---
+++ identify_corners.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+-----------------------------------------------------------------------------
+Submission for the SRC FTBL take-home task for the Data Scientist position
+  Identifies possible corner kick situations in the given SkillCorner dataset
+-----------------------------------------------------------------------------
+:author: Emily Chen
+:date:   May 2023
+
+Script contains two helper functions:
+  1. identify_corner_kicks()
+      A rule-based model that identifies possible corner kicks,
+      given the x-y coordinates of the soccer ball
+
+  2. write_out_to_csv()
+      Writes the output of identify_corner_kicks() to a three-column csv file
+      listing the 'frame', the corner-taking 'team', and the 'time'
+
+Outputs a directory 'corners' that stores the csv file for each game
+
+REQUIREMENTS: pandas
+
+USAGE: python3 identify_corners.py
+
+'''
+import csv
+import glob
+import json
+import os
+
+import pandas as pd
+
+
+# for debugging purposes
+pd.set_option('display.max_columns', None)
+
+def identify_corner_kicks(df):
+    '''
+    :param df: all the data stored in 'structured_data.json'
+    :type  df: pandas dataframe
+
+    :return: possible_corners
+    :rtype:  list of pandas dataframes
+
+    A rule-based model that identifies possible corner kicks,
+    given the x-y coordinates of the soccer ball
+
+    '''
+    possible_corners = []
+
+    for frame in range(len(df)):
+    
+        tracking_data = df['data'].iloc[frame]
+    
+        # check if there's tracking data
+        if tracking_data and any("trackable_object" in elem for elem in tracking_data):
+    
+#            num_obj = 0
+
+            # check if there's a trackable object and if object is the ball
+            for elem in tracking_data:
+                if "trackable_object" in elem and elem['trackable_object'] == 55:
+    
+                    # absolute value of the x-y coordinates of the ball
+                    x_coord = abs(elem['x'])
+                    y_coord = abs(elem['y'])
+                    radius  = 1.2
+
+                    # pythagorean theorem for circles
+                    #   checks if the ball is within radius of corner arc 
+                    #   with some leeway given to include radius of the ball
+                    if (x_coord - 52.5)**2 + (y_coord - 34)**2 <= radius:
+                        possible_corners.append(df.iloc[frame])
+
+#                # check number of trackable objects within 18-yard box
+#                if "trackable_object" in elem:
+#                    x_coord = abs(elem['x'])
+#                    y_coord = abs(elem['y'])
+#
+#                    if x_coord >= 36 and x_coord <= 52.5 and \
+#                       y_coord >= 0  and y_coord <= 21.16:
+#                        num_obj += 1
+#
+#            if num_obj >= 16:
+#                possible_corners.append(df.iloc[frame])
+
+    return possible_corners
+
+
+def write_out_to_csv(dirname, match_data, corners):
+    '''
+    :param dirname: path to folder in which to store csv
+    :type  dirname: str
+    :param match_data: all the data stored in 'match_data.json'
+    :type  match_data: pandas dataframe
+    :param corners: possible corner kick situations
+    :type  corners: list of pandas dataframes
+
+    Writes the output of identify_corner_kicks() to a three-column csv file
+    listing the 'frame', the corner-taking 'team', and the 'time'
+
+    '''
+    date      = str(match_data['date_time'])
+    home_team = match_data['home_team']['name'].replace(" ","_")
+    away_team = match_data['away_team']['name'].replace(" ","_")
+    match_id  = str(match_data['id'])
+   
+    filename  = dirname + "/" + match_id + "_" + home_team + "(H)_" + \
+               away_team + "(A)_" + date + ".csv"
+
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Frame", "Team", "Time"])
+
+        for df in corners:
+            if df['possession']['group'] == "home team":
+                writer.writerow([df['frame'], home_team, df['time']])
+            elif df['possession']['group'] == "away team":
+                writer.writerow([df['frame'], away_team, df['time']])
+            else:
+                writer.writerow([df['frame'], "n/a", df['time']])
+
+
+def main():
+
+    # creates a folder 'corners' to store output, i.e.
+    #   csv files listing frames and time stamps of
+    #   the corner kicks from each game
+    dirname = 'corners'
+    if os.path.exists(dirname):
+        for root, dirs, files in os.walk(dirname):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(dirname)
+    os.mkdir(dirname)
+
+#    for jsonfile in glob.glob('data/matches/2440/*.json'):
+    for jsonfile in glob.glob('data/matches/*/*.json'):
+        with open(jsonfile, 'r') as json_data:
+
+            basename = jsonfile.rsplit("/", 1)[1]
+
+            if basename == "match_data.json":
+                match_data = json.load(json_data)
+    
+            corner_kicks = []
+            if basename == "structured_data.json":
+                tracking_data = pd.read_json(json_data)
+                corner_kicks  = identify_corner_kicks(tracking_data)
+            
+            write_out_to_csv(dirname, match_data, corner_kicks)
+
+
+if __name__ == "__main__":
+    main()
